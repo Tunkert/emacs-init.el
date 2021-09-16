f32b624c69b7ee75ef6f03dbaa355102d6a8679a
Timothy Unkert <45219053+Tunkert@users.noreply.github.com>
2021-09-16 12:59:41 -0600

    Updated init.el for current setup

--- init.el
+++ init.el
@@ -1,36 +1,121 @@
-;; get rid of welcome screen on startup
+(setq inhibit-startup-message t)
 
-;; Added by Package.el.  This must come before configurations of
-;; installed packages.  Don't delete this line.  If you don't want it,
-;; just comment it out by adding a semicolon to the start of the line.
-;; You may delete these explanatory comments.
-(package-initialize)
+(scroll-bar-mode -1)
+(tool-bar-mode -1)
+(tooltip-mode -1)
+(set-fringe-mode 10)
 
-(setq inhibit-splash-screen t)
-
-;; set default font and size
-(set-default-font "Cousine 12")
-
-;; maximize screen on startup
-(add-hook 'window-setup-hook 'toggle-frame-maximized t)
-
-;; disable menu on startup
 (menu-bar-mode -1)
 
-;; disable tools on startup
-(tool-bar-mode -1)
+(load-theme 'doom-shades-of-purple t)
 
-;; disable scroll bar on startup
-(toggle-scroll-bar -1)
+(add-to-list 'default-frame-alist '(fullscreen . maximized))
 
-;; change theme
-(add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
-(load-theme 'atom-one-dark t)
+(set-face-attribute 'default nil :font "Cousine-11" )
 
-;; emmet mode
-(add-to-list 'load-path "~/.emacs.d/emmet-mode/")
-(require 'emmet-mode)
+(require 'package)
 
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+			 ("melpa-stable" . "https://stable.melpa.org/packages/")
+			 ("elpa" . "https://elpa.gnu.org/packages/")))
 
+(package-initialize)
+(unless package-archive-contents
+  (package-refresh-contents))
 
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package))
 
+(require 'use-package)
+(setq use-package-always-ensure t)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector
+   ["#2D2A2E" "#CC6666" "#A9DC76" "#FFD866" "#78DCE8" "#FF6188" "#78DCE8" "#FCFCFA"])
+ '(custom-safe-themes
+   '("234dbb732ef054b109a9e5ee5b499632c63cc24f7c2383a849815dacc1727cb6" "e8df30cd7fb42e56a4efc585540a2e63b0c6eeb9f4dc053373e05d774332fc13" "cf922a7a5c514fad79c483048257c5d8f242b21987af0db813d3f0b138dfaf53" "4f1d2476c290eaa5d9ab9d13b60f2c0f1c8fa7703596fa91b235db7f99a9441b" "a9a67b318b7417adbedaab02f05fa679973e9718d9d26075c6235b1f0db703c8" "0b3aee906629ac7c3bd994914bf252cf92f7a8b0baa6d94cb4dfacbd4068391d" "c5ded9320a346146bbc2ead692f0c63be512747963257f18cc8518c5254b7bf5" "7a7b1d475b42c1a0b61f3b1d1225dd249ffa1abb1b7f726aec59ac7ca3bf4dae" "bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" "b0e446b48d03c5053af28908168262c3e5335dcad3317215d9fdeb8bac5bacf9" "97db542a8a1731ef44b60bc97406c1eb7ed4528b0d7296997cbb53969df852d6" "7eea50883f10e5c6ad6f81e153c640b3a288cd8dc1d26e4696f7d40f754cc703" default))
+ '(exwm-floating-border-color "#413f42")
+ '(fci-rule-color "#4C4A4D")
+ '(highlight-tail-colors ((("#393b35") . 0) (("#343b40") . 20)))
+ '(hl-todo-keyword-faces
+   '(("TODO" . "#dc752f")
+     ("NEXT" . "#dc752f")
+     ("THEM" . "#2d9574")
+     ("PROG" . "#4f97d7")
+     ("OKAY" . "#4f97d7")
+     ("DONT" . "#f2241f")
+     ("FAIL" . "#f2241f")
+     ("DONE" . "#86dc2f")
+     ("NOTE" . "#b1951d")
+     ("KLUDGE" . "#b1951d")
+     ("HACK" . "#b1951d")
+     ("TEMP" . "#b1951d")
+     ("FIXME" . "#dc752f")
+     ("XXX+" . "#dc752f")
+     ("\\?\\?\\?+" . "#dc752f")))
+ '(jdee-db-active-breakpoint-face-colors (cons "#19181A" "#FCFCFA"))
+ '(jdee-db-requested-breakpoint-face-colors (cons "#19181A" "#A9DC76"))
+ '(jdee-db-spec-breakpoint-face-colors (cons "#19181A" "#727072"))
+ '(objed-cursor-color "#CC6666")
+ '(package-selected-packages
+   '(magit key-chord evil-visual-mark-mode spacemacs-theme doom-themes dracula-theme prettier web-mode php-mode emmet-mode use-package))
+ '(pdf-view-midnight-colors (cons "#FCFCFA" "#2D2A2E"))
+ '(rustic-ansi-faces
+   ["#2D2A2E" "#CC6666" "#A9DC76" "#FFD866" "#78DCE8" "#FF6188" "#78DCE8" "#FCFCFA"])
+ '(vc-annotate-background "#2D2A2E")
+ '(vc-annotate-color-map
+   (list
+    (cons 20 "#A9DC76")
+    (cons 40 "#c5da70")
+    (cons 60 "#e2d96b")
+    (cons 80 "#FFD866")
+    (cons 100 "#fec266")
+    (cons 120 "#fdad66")
+    (cons 140 "#FC9867")
+    (cons 160 "#fd8572")
+    (cons 180 "#fe737d")
+    (cons 200 "#FF6188")
+    (cons 220 "#ee627c")
+    (cons 240 "#dd6471")
+    (cons 260 "#CC6666")
+    (cons 280 "#b56869")
+    (cons 300 "#9f6b6c")
+    (cons 320 "#886d6f")
+    (cons 340 "#4C4A4D")
+    (cons 360 "#4C4A4D")))
+ '(vc-annotate-very-old-color nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+(defun my-web-mode-hook ()
+  "Hooks for Web mode."
+  (setq web-mode-markup-indent-offset 2)
+  (setq web-mode-css-indent-offset 2)
+  (setq web-mode-code-indent-offset 2)
+)
+(add-hook 'web-mode-hook  'my-web-mode-hook)
+
+(require 'web-mode)
+(add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.[agj]sp\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
+
+;; Enable Evil
+(require 'evil)
+(evil-mode 1)
+
+;;Exit insert mode by pressing j and then j quickly
+(setq key-chord-two-keys-delay 0.5)
+(key-chord-define evil-insert-state-map "jj" 'evil-normal-state)
+(key-chord-mode 1)
