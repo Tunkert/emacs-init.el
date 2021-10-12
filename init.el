bd77ac70914000893f9018a58b9c2f3cacb40229
Timothy Unkert <45219053+Tunkert@users.noreply.github.com>
2021-10-11 18:00:49 -0600

    Latest init.el

--- init.el
+++ init.el
@@ -16,16 +16,32 @@
 ;; disable scroll bar on startup
 (toggle-scroll-bar -1)
 
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(package-initialize)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (auto-complete key-chord gruvbox-theme evil-visual-mark-mode dot-mode web-mode emmet-mode))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
 ;; change theme
-(add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
-(load-theme 'dracula t)
+(load-theme 'gruvbox t)
 
 ;; emmet mode
-(add-to-list 'load-path "~/.emacs.d/emmet-mode/")
 (require 'emmet-mode)
 
 ;; web mode
-(add-to-list 'load-path "~/.emacs.d/web-mode/")
 (require 'web-mode)
 (add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.php\\'" . web-mode))
@@ -35,6 +51,8 @@
 (add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.scss\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.css\\'" . web-mode))
 
 (defun my-web-mode-hook ()
   "Hooks for Web mode."
@@ -44,8 +62,16 @@
 )
 (add-hook 'web-mode-hook  'my-web-mode-hook)
 
+;; evil mode
+(require 'evil)
+(evil-mode 1)
+
+;;Exit insert mode by pressing j and then j quickly
+(setq key-chord-two-keys-delay 0.5)
+(key-chord-define evil-insert-state-map "jj" 'evil-normal-state)
+(key-chord-mode 1)
+
 ;; dot mode
-(add-to-list 'load-path "~/.emacs.d/dot-mode/")
 (require 'dot-mode)
 (global-dot-mode t)
 
@@ -55,3 +81,6 @@
 
 ;; activate line numbering in all buffers/modes
 (global-display-line-numbers-mode) 
+
+;; auto complete
+(ac-config-default)
