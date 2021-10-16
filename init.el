ac8224b91b91bc16d731e69320a737cefee4b441
Timothy Unkert <45219053+Tunkert@users.noreply.github.com>
2021-10-15 21:31:08 -0600

    Updated init.el for google-translate

--- init.el
+++ init.el
@@ -1,8 +1,11 @@
+;; inihibit cl warning
+(setq byte-compile-warnings '(cl-functions))
+
 ;; inhibit splash screen
 (setq inhibit-splash-screen t)
 
 ;; set default font and size
-(set-default-font "Cousine 11")
+(set-face-attribute 'default nil :font "Cousine-11" )
 
 ;; maximize screen on startup
 (add-hook 'window-setup-hook 'toggle-frame-maximized t)
@@ -18,6 +21,7 @@
 
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
 (package-initialize)
 
 (custom-set-variables
@@ -25,9 +29,44 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector
+   ["#292D3E" "#ff5370" "#c3e88d" "#ffcb6b" "#82aaff" "#c792ea" "#89DDFF" "#EEFFFF"])
+ '(custom-safe-themes
+   '("a0be7a38e2de974d1598cf247f607d5c1841dbcef1ccd97cded8bea95a7c7639" default))
+ '(exwm-floating-border-color "#232635")
+ '(fci-rule-color "#676E95")
+ '(highlight-tail-colors ((("#383f45") . 0) (("#323e51") . 20)))
+ '(jdee-db-active-breakpoint-face-colors (cons "#1c1f2b" "#c792ea"))
+ '(jdee-db-requested-breakpoint-face-colors (cons "#1c1f2b" "#c3e88d"))
+ '(jdee-db-spec-breakpoint-face-colors (cons "#1c1f2b" "#676E95"))
+ '(objed-cursor-color "#ff5370")
  '(package-selected-packages
-   (quote
-    (auto-complete key-chord gruvbox-theme evil-visual-mark-mode dot-mode web-mode emmet-mode))))
+   '(ob-translate prettier ac-js2 ac-math ac-html-bootstrap ac-php php-mode auto-complete neotree doom-themes emmet-mode web-mode key-chord evil-visual-mark-mode))
+ '(pdf-view-midnight-colors (cons "#EEFFFF" "#292D3E"))
+ '(rustic-ansi-faces
+   ["#292D3E" "#ff5370" "#c3e88d" "#ffcb6b" "#82aaff" "#c792ea" "#89DDFF" "#EEFFFF"])
+ '(vc-annotate-background "#292D3E")
+ '(vc-annotate-color-map
+   (list
+    (cons 20 "#c3e88d")
+    (cons 40 "#d7de81")
+    (cons 60 "#ebd476")
+    (cons 80 "#ffcb6b")
+    (cons 100 "#fcb66b")
+    (cons 120 "#f9a16b")
+    (cons 140 "#f78c6c")
+    (cons 160 "#e78e96")
+    (cons 180 "#d690c0")
+    (cons 200 "#c792ea")
+    (cons 220 "#d97dc1")
+    (cons 240 "#ec6898")
+    (cons 260 "#ff5370")
+    (cons 280 "#d95979")
+    (cons 300 "#b36082")
+    (cons 320 "#8d678b")
+    (cons 340 "#676E95")
+    (cons 360 "#676E95")))
+ '(vc-annotate-very-old-color nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -36,7 +75,7 @@
  )
 
 ;; change theme
-(load-theme 'gruvbox t)
+(load-theme 'doom-palenight t)
 
 ;; emmet mode
 (require 'emmet-mode)
@@ -71,10 +110,6 @@
 (key-chord-define evil-insert-state-map "jj" 'evil-normal-state)
 (key-chord-mode 1)
 
-;; dot mode
-(require 'dot-mode)
-(global-dot-mode t)
-
 ;; Enable line numbers
 ;; set type of line numbering (global variable)
 (setq display-line-numbers-type 'relative) 
@@ -84,3 +119,55 @@
 
 ;; auto complete
 (ac-config-default)
+
+;; neo tree config
+(add-to-list 'load-path "/some/path/neotree")
+(require 'neotree)
+(global-set-key [f8] 'neotree-toggle)
+
+(add-hook 'neotree-mode-hook
+              (lambda ()
+                (define-key evil-normal-state-local-map (kbd "TAB") 'neotree-enter)
+                (define-key evil-normal-state-local-map (kbd "SPC") 'neotree-quick-look)
+                (define-key evil-normal-state-local-map (kbd "q") 'neotree-hide)
+                (define-key evil-normal-state-local-map (kbd "RET") 'neotree-enter)
+                (define-key evil-normal-state-local-map (kbd "g") 'neotree-refresh)
+                (define-key evil-normal-state-local-map (kbd "n") 'neotree-next-line)
+                (define-key evil-normal-state-local-map (kbd "p") 'neotree-previous-line)
+                (define-key evil-normal-state-local-map (kbd "A") 'neotree-stretch-toggle)
+                (define-key evil-normal-state-local-map (kbd "H") 'neotree-hidden-file-toggle)))
+
+(defun bs-php-mode-hook ()
+  (setq indent-tabs-mode nil)
+  (setq c-basic-offset 2)
+  (setq php-template-compatibility nil)
+  (subword-mode 1))
+
+(add-hook 'php-mode-hook 'bs-php-mode-hook)
+
+(defun toggle-php-flavor-mode ()
+  (interactive)
+  "Toggle mode between PHP & Web-Mode Helper modes"
+  (cond ((string= mode-name "PHP")
+         (web-mode))
+        ((string= mode-name "Web")
+         (php-mode))))
+
+(global-set-key [f5] 'toggle-php-flavor-mode)
+
+(defun bs-php-mode-hook ()
+  (auto-complete-mode t)                 ;; «
+  (require 'ac-php)                      ;; «
+  (setq ac-sources  '(ac-source-php ))   ;; «
+  (yas-global-mode 1)                    ;; «
+  (setq indent-tabs-mode nil)
+  (setq php-template-compatibility nil)
+  (setq c-basic-offset 2))
+
+;; Google Translate
+(require 'google-translate)
+(require 'google-translate-default-ui)
+(global-set-key "\C-ct" 'google-translate-at-point)
+(global-set-key "\C-cT" 'google-translate-query-translate)
+(defun google-translate--search-tkk () "Search TKK." (list 430675 2721866130))
+(setq google-translate-backend-method 'curl)
