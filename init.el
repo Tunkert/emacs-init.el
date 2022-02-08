abe036f7472bf9fe76c15af8b34ea479dbf87457
Timothy Unkert <45219053+Tunkert@users.noreply.github.com>
2022-02-07 20:39:17 -0700

    init.el new setup for front end dev

--- init.el
+++ init.el
@@ -2,7 +2,7 @@
 (setq inhibit-splash-screen t)
 
 ;; set default font and size
-(set-face-attribute 'default nil :font "Cousine-11" )
+(set-face-attribute 'default nil :font "Inconsolata-11" )
 
 ;; maximize screen on startup
 (add-hook 'window-setup-hook 'toggle-frame-maximized t)
@@ -16,31 +16,88 @@
 ;; disable scroll bar on startup
 (toggle-scroll-bar -1)
 
-(require 'package)
-(add-to-list 'package-archives
-             '("melpa" . "http://melpa.org/packages/") t)
-(package-initialize)
-
+;; line numbers
 (global-display-line-numbers-mode t)
 (setq display-line-numbers-type 'relative)
 
+;; auto-closing pairs
 (electric-pair-mode 1)
 
 ;; no backup files
 (setq make-backup-files nil)
 
-;; custom theme load path
-(add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
+;; melpa
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.org/packages/") t)
+
+;; tabs setting
+(setq tab-width 2)
+(setq js-indent-level 2)
+(setq js-indent-offset 2)
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector
+   [default bold shadow italic underline bold bold-italic bold])
+ '(ansi-color-names-vector
+   ["#212526" "#ff4b4b" "#b4fa70" "#fce94f" "#729fcf" "#e090d7" "#8cc4ff" "#eeeeec"])
+ '(custom-enabled-themes nil)
  '(custom-safe-themes
-   '("bf7f4fb05a45eae1a6bc1a009b7731b09260d945ec4c3c4ed7f5da06647a7946" "1b780020c8fe8c91829c06d2a9d5c7d8a182216e479c5b24e787fb1712ffb176" "3b050147ba211a3711c5421d8c98078b75e983261873548e36fe477d83443b56" default)))
+   '("22f080367d0b7da6012d01a8cd672289b1debfb55a76ecdb08491181dcb29626" "90a6f96a4665a6a56e36dec873a15cbedf761c51ec08dd993d6604e32dd45940" "1d78d6d05d98ad5b95205670fe6022d15dabf8d131fe087752cc55df03d88595" "171d1ae90e46978eb9c342be6658d937a83aaa45997b1d7af7657546cae5985b" default))
+ '(fci-rule-color "#969896")
+ '(hl-sexp-background-color "#1c1f26")
+ '(nrepl-message-colors
+   '("#183691" "#969896" "#a71d5d" "#969896" "#0086b3" "#795da3" "#a71d5d" "#969896"))
+ '(package-selected-packages
+   '(dracula-theme emmet-mode web-mode js2-mode github-theme atom-one-dark-theme material-theme))
+ '(pdf-view-midnight-colors '("#969896" . "#f8eec7"))
+ '(tetris-x-colors
+   [[229 192 123]
+    [97 175 239]
+    [209 154 102]
+    [224 108 117]
+    [152 195 121]
+    [198 120 221]
+    [86 182 194]])
+ '(vc-annotate-background "#b0cde7")
+ '(vc-annotate-color-map
+   '((20 . "#969896")
+     (40 . "#183691")
+     (60 . "#969896")
+     (80 . "#969896")
+     (100 . "#969896")
+     (120 . "#a71d5d")
+     (140 . "#969896")
+     (160 . "#969896")
+     (180 . "#969896")
+     (200 . "#969896")
+     (220 . "#63a35c")
+     (240 . "#0086b3")
+     (260 . "#795da3")
+     (280 . "#969896")
+     (300 . "#0086b3")
+     (320 . "#969896")
+     (340 . "#a71d5d")
+     (360 . "#969896")))
+ '(vc-annotate-very-old-color "#969896"))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+;; web mode
+(require 'web-mode)
+(add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.js\\'" . web-mode))
+
+;; web mode indenting
+(setq web-mode-markup-indent-offset 2)
+(setq web-mode-css-indent-offset 2)
+(setq web-mode-code-indent-offset 2)
+(setq web-mode-attr-indent-offset 2)
