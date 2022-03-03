48338c4f5ae643647c87523e47894ba2d0903135
Timothy Unkert <45219053+Tunkert@users.noreply.github.com>
2022-03-03 09:46:32 -0700

    new init with export to html 5 for org

--- init.el
+++ init.el
@@ -2,7 +2,7 @@
 (setq inhibit-splash-screen t)
 
 ;; set default font and size
-(set-face-attribute 'default nil :font "Cousine-11" )
+(set-face-attribute 'default nil :font "Monaco-11" )
 
 ;; disable menu on startup
 (menu-bar-mode -1)
@@ -19,6 +19,18 @@
 
 ;; auto-closing pairs
 (electric-pair-mode 1)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes '(light-blue)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
 
-;; no backup files
-(setq make-backup-files nil)
+;; org mode html 5
+(setq org-html-doctype "html5")
