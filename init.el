bf9070627abbac41061971ba614e64511712ac8c
Timothy Unkert <45219053+Tunkert@users.noreply.github.com>
2022-03-14 18:59:35 -0600

    updated with doom modeline

--- init.el
+++ init.el
@@ -2,7 +2,7 @@
 (setq inhibit-splash-screen t)
 
 ;; set default font and size
-(set-face-attribute 'default nil :font "Monaco-11" )
+(set-face-attribute 'default nil :font "SourceCodePro-11" )
 
 ;; disable menu on startup
 (menu-bar-mode -1)
@@ -19,12 +19,19 @@
 
 ;; auto-closing pairs
 (electric-pair-mode 1)
+
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-enabled-themes '(light-blue)))
+ '(custom-enabled-themes '(doom-gruvbox))
+ '(custom-safe-themes
+   '("c4063322b5011829f7fdd7509979b5823e8eea2abf1fe5572ec4b7af1dd78519" default))
+ '(package-selected-packages
+   '(web-mode js2-mode doom-modeline doom-themes all-the-icons)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -32,5 +39,26 @@
  ;; If there is more than one, they won't work right.
  )
 
-;; org mode html 5
-(setq org-html-doctype "html5")
+(when (display-graphic-p)
+  (require 'all-the-icons))
+
+(require 'doom-modeline)
+(doom-modeline-mode 1)
+
+(require 'web-mode)
+(add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.[agj]sp\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+
+(defun my-web-mode-hook ()
+  "Hooks for Web mode."
+  (setq web-mode-markup-indent-offset 2)
+  (setq web-mode-css-indent-offset 2)
+  (setq web-mode-code-indent-offset 2)
+)
+(add-hook 'web-mode-hook  'my-web-mode-hook)
