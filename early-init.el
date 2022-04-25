0c8a967dcaa5cec9e56a957c41b4710eb5b48f36
Timothy Unkert <45219053+Tunkert@users.noreply.github.com>
2022-04-25 08:54:33 -0600

    new early-init and init

--- early-init.el
+++ early-init.el
@@ -1,14 +1,12 @@
-;; start the initial frame maximized
-(add-to-list 'initial-frame-alist '(fullscreen . maximized))
-
-;; start every frame maximized
-(add-to-list 'default-frame-alist '(fullscreen . maximized))
+;; initial frame size
+(add-to-list 'default-frame-alist '(height . 45))
+(add-to-list 'default-frame-alist '(width . 130))
 
 ;; inhibit splash screen
 (setq inhibit-splash-screen t)
 
 ;; set default font and size
-(set-face-attribute 'default nil :font "Cousine-11" )
+(set-face-attribute 'default nil :font "Hermit-11" )
 
 ;; disable menu on startup
 (menu-bar-mode -1)
